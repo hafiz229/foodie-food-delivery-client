af50715c921a92aa4485cbfae0179bd521f0a72b
Hafiz <hasnathafiz229>
2021-10-31 02:28:28 +0600

    menus added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import AboutUs from "../AboutUs/AboutUs";
 import Banner from "../Banner/Banner";
 import Footer from "../Footer/Footer";
+import Menus from "../Menus/Menus";
 import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Menus></Menus>
       <AboutUs></AboutUs>
       <Testimonials></Testimonials>
       <Footer></Footer>
---
+++ src/Pages/Menus/Menus.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Menus.css";
+
+const Menus = () => {
+  return (
+    <div>
+      <h1 className="py-3">Our Menus</h1>
+      <div className="container menu-container px-3 d-lg-flex justify-content-center align-items-center">
+        <div class="card menu-border shadow" style={{ width: "18rem" }}>
+          <img
+            style={{ height: "200px" }}
+            src="https://image.freepik.com/free-vector/sticker-template-with-pizza-isolated_1308-62307.jpg"
+            class="card-img-top rounded-circle menu-img p-3"
+            alt="..."
+          />
+          <div class="card-body menu-item">
+            <p class="card-text">Pizza</p>
+          </div>
+        </div>
+        <div class="card menu-border shadow" style={{ width: "18rem" }}>
+          <img
+            style={{ height: "200px" }}
+            src="https://image.freepik.com/free-vector/hamburger_53876-25481.jpg"
+            class="card-img-top rounded-circle menu-img p-3"
+            alt="..."
+          />
+          <div class="card-body menu-item">
+            <p class="card-text">Burger</p>
+          </div>
+        </div>
+        <div class="card menu-border shadow" style={{ width: "18rem" }}>
+          <img
+            style={{ height: "200px" }}
+            src="https://image.freepik.com/free-vector/flying-fried-chicken-with-bucket-cartoon_138676-2081.jpg"
+            class="card-img-top rounded-circle menu-img p-3"
+            alt="..."
+          />
+          <div class="card-body menu-item">
+            <p class="card-text">Fried Chicken</p>
+          </div>
+        </div>
+        <div class="card menu-border shadow" style={{ width: "18rem" }}>
+          <img
+            style={{ height: "200px" }}
+            src="https://image.freepik.com/free-vector/modern-juice-colourfull-logo_91719-70.jpg"
+            class="card-img-top rounded-circle menu-img p-3"
+            alt="..."
+          />
+          <div class="card-body menu-item">
+            <p class="card-text">Drink</p>
+          </div>
+        </div>
+        <div class="card menu-border shadow" style={{ width: "18rem" }}>
+          <img
+            style={{ height: "200px" }}
+            src="https://image.freepik.com/free-vector/breakfast-sandwich-top-view-realistic-image_1284-14462.jpg"
+            class="card-img-top rounded-circle menu-img p-3"
+            alt="..."
+          />
+          <div class="card-body menu-item">
+            <p class="card-text">Sandwich</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menus;
