3031713b9d0e53ba77a1758e0cd8f9267702fb59
Hafiz <hasnathafiz229>
2021-10-31 00:54:37 +0600

    footer added

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="bg-light text-black text-center p-3 mb-0 fw-bold">
+        <span>Foodie</span> © Copyright 2024
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
